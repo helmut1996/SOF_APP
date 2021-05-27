ff3564ebfd8057f46da56630ca5c0f6e9121759c
helmut josue colindres Brenes <helmut-1521@hotmail.com>
2021-05-27 13:10:07 -0600

    Agregando diseño login

--- lib/src/pages/HomePages.dart
+++ lib/src/pages/HomePages.dart
@@ -1,83 +1,34 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:hidden_drawer_menu/hidden_drawer_menu.dart';
-import 'package:sof_app/src/pages/FacturasPages.dart';
-import 'package:sof_app/src/pages/PreFacturasPages.dart';
-import 'package:sof_app/src/pages/ProductoPages.dart';
 
-class HomePages extends StatefulWidget {
-  @override
-  _HomePagesState createState() => _HomePagesState();
-}
-
-class _HomePagesState extends State<HomePages> {
-  final List<ScreenHiddenDrawer> itemList = [
-    ScreenHiddenDrawer(
-      ItemHiddenMenu(
-        name: "Facturas",
-        colorLineSelected: Colors.white,
-        baseStyle:
-            TextStyle(color: Colors.white.withOpacity(0.8), fontSize: 25.0),
-        selectedStyle: TextStyle(color: Colors.black54),
-      ),
-      FacturasPages(),
-    ),
-    ScreenHiddenDrawer(
-      ItemHiddenMenu(
-        name: "PreFacturas",
-        colorLineSelected: Colors.white,
-        baseStyle:
-            TextStyle(color: Colors.white.withOpacity(0.8), fontSize: 25.0),
-        selectedStyle: TextStyle(color: Colors.white),
-        onTap: () {
-          print("Click item");
-        },
-      ),
-      PreFacturasPages(),
-    ),
-    ScreenHiddenDrawer(
-      ItemHiddenMenu(
-        name: "Productos",
-        colorLineSelected: Colors.white,
-        baseStyle:
-            TextStyle(color: Colors.white.withOpacity(0.8), fontSize: 25.0),
-        selectedStyle: TextStyle(color: Colors.white),
-        onTap: () {
-          print("Click item");
-        },
-      ),
-      ProductoPages(),
-    )
-  ];
-
+class HomePages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return HiddenDrawerMenu(
-      backgroundColorMenu: Colors.blueGrey,
-      backgroundColorAppBar: Colors.pinkAccent,
-      screens: itemList,
-      //    typeOpen: TypeOpen.FROM_RIGHT,
-      //    disableAppBarDefault: false,
-      //    enableScaleAnimin: true,
-      //    enableCornerAnimin: true,
-      //    slidePercent: 80.0,
-      //    verticalScalePercent: 80.0,
-      //    contentCornerRadius: 10.0,
-      //    iconMenuAppBar: Icon(Icons.menu),
-      //    backgroundContent: DecorationImage((image: ExactAssetImage('assets/bg_news.jpg'),fit: BoxFit.cover),
-      //    whithAutoTittleName: true,
-      //    styleAutoTittleName: TextStyle(color: Colors.red),
-      //    actionsAppBar: <Widget>[],
-      //    backgroundColorContent: Colors.blue,
-      //    elevationAppBar: 4.0,
-      tittleAppBar: Center(
-          //child: Icon(Icons.ac_unit),
-          ),
-      //    enableShadowItensMenu: true,
-      backgroundMenu: DecorationImage(
-          image: NetworkImage(
-              'https://firebasestorage.googleapis.com/v0/b/prueba2-8bf0c.appspot.com/o/logoushas.jpg?alt=media&token=1ff0bc30-1ab8-47b0-9e3f-a770b95e3e9f'),
-          fit: BoxFit.fill),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+              height: 400,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/imagenes/fondo.png'),
+                ),
+              ),
+            ),
+            Center(
+              child: Positioned(
+                  child: Container(
+                width: 100,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('assets/imagenes/kool.jpg'))),
+              )),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
