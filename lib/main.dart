9ce5b98b309b57638125777cdb118dc32d45d39e
helmut josue colindres Brenes <helmut-1521@hotmail.com>
2021-05-28 10:04:23 -0600

    Implementando patron Bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:sof_app/src/pages/HomePages.dart';
+import 'package:sof_app/Presentation/pages/HomePages.dart';
 
 void main() => runApp(MyApp());
 
