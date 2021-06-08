f3a16631bb27a21e5e6d2dfe9d3356763e97c7a5
helmut josue colindres Brenes <helmut-1521@hotmail.com>
2021-06-08 15:09:03 -0600

    error correguido

--- lib/Routes/Router.dart
+++ lib/Routes/Router.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:sof_app/Presentation/pages/FacturasCosmeticoPages.dart';
-import 'package:sof_app/Presentation/pages/FacturasFerreteriaPages.dart';
-import 'package:sof_app/Presentation/pages/FacturasLibrerias.dart';
 import 'package:sof_app/Presentation/pages/HomePages.dart';
 import 'package:sof_app/Presentation/pages/LoginPages.dart';
-import 'package:sof_app/Presentation/pages/PreFacturasCosmeticosPages.dart';
 
 class Routes {
   static Route<dynamic> generateRoute(RouteSettings settings) {
@@ -15,18 +11,6 @@ class Routes {
         return MaterialPageRoute(builder: (_) => HomePages());
       case '/login':
         return MaterialPageRoute(builder: (_) => LoginPages());
-
-      case '/facturasCosmeticos':
-        return MaterialPageRoute(builder: (_) => FacturaFerreteriaPages());
-
-      case '/Prefacturas':
-        return MaterialPageRoute(builder: (_) => PreFacturasCosmeticoPages());
-
-      case '/FacturasFerreteria':
-        return MaterialPageRoute(builder: (_) => FacturaCosmeticoPages());
-
-      case '/FacturasLibreria':
-        return MaterialPageRoute(builder: (_) => FacturaLibreriaPages());
 
       default:
         return MaterialPageRoute(
