a98edb03b225d6bc831cd1ae28c42817a12854d2
helmut josue colindres Brenes <helmut-1521@hotmail.com>
2021-06-29 11:07:43 -0600

    union de widget en detalle de la factura

--- lib/Presentation/pages/DetailsFactura.dart
+++ lib/Presentation/pages/DetailsFactura.dart
@@ -1,10 +1,59 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:sof_app/Presentation/pages/Wiggets/Components.dart';
+import 'package:sof_app/Presentation/pages/HomePages.dart';
+import 'package:sof_app/provaiders/models/modelDetalleFactura.dart';
+import 'package:http/http.dart' as http;
+import 'package:async/async.dart';
+import 'package:sof_app/provaiders/models/modelFacturaCosmetico.dart';
+
+final _DetalleFacturaCosmetico = AsyncMemoizer<DetalleFacturaCosmeticos>();
+final _DetalleFacturaCarpintero = AsyncMemoizer<DetalleFacturaCosmeticos>();
+final _DetalleFacturaLibreria = AsyncMemoizer<DetalleFacturaCosmeticos>();
+
+//es la clase para la conexion de la api DetalleFacturaCosmeticos
+Future<DetalleFacturaCosmeticos> fetchDetalleFacturaCosmetico(int idFactura) =>
+    _DetalleFacturaCosmetico.runOnce(() async {
+      final response = await http.get(Uri.parse(
+          "https://apimarnor.garajestore.com/Apifacturas/info_factura_cosmetico/" +
+              idFactura.toString() +
+              "/?token=ifKZ56rMQdOKmWuDHF"));
+      if (response.statusCode == 200) {
+        return compute(ParsedetalleFacturaCosmeticos, response.body);
+      } else {
+        throw Exception("Failed to load.");
+      }
+    });
+
+//es la clase para la conexion de la api DetalleFacturaCarpintero
+Future<DetalleFacturaCosmeticos> fetchDetalleFacturaCarpintero(int idFactura) =>
+    _DetalleFacturaCarpintero.runOnce(() async {
+      final response = await http.get(Uri.parse(
+          "https://apimarnor.garajestore.com/Apifacturas/info_factura_carpintero/" +
+              idFactura.toString() +
+              "/?token=ifKZ56rMQdOKmWuDHF"));
+      if (response.statusCode == 200) {
+        return compute(ParsedetalleFacturaCosmeticos, response.body);
+      } else {
+        throw Exception("Failed to load.");
+      }
+    });
+
+//es la clase para la conexion de la api DetalleFacturaLibreria
+Future<DetalleFacturaCosmeticos> fetchDetalleFacturaLibreria(int idFactura) =>
+    _DetalleFacturaLibreria.runOnce(() async {
+      final response = await http.get(Uri.parse(
+          "https://apimarnor.garajestore.com/Apifacturas/info_factura_libreria/" +
+              idFactura.toString() +
+              "/?token=ifKZ56rMQdOKmWuDHF"));
+      if (response.statusCode == 200) {
+        return compute(ParsedetalleFacturaCosmeticos, response.body);
+      } else {
+        throw Exception("Failed to load.");
+      }
+    });
 
 class DetailsFactura extends StatefulWidget {
-  const DetailsFactura({Key? key}) : super(key: key);
-
   @override
   _DetailsFacturaState createState() => _DetailsFacturaState();
 }
@@ -12,13 +61,113 @@ class DetailsFactura extends StatefulWidget {
 class _DetailsFacturaState extends State<DetailsFactura> {
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    final double itemHeight = (size.height - kToolbarHeight) / 19;
+    final double itemWidth = size.width / 2;
     return Scaffold(
       appBar: AppBar(
-        title: Text("Factura no. 110256"),
+        title: Text("No.Factura"),
       ),
       body: Column(
         children: [
-          Textos(),
+          Column(
+            children: [
+              Divider(),
+              GridView.count(
+                crossAxisCount: 2,
+                physics:
+                    NeverScrollableScrollPhysics(), // to disable GridView's scrolling
+                shrinkWrap: true,
+                childAspectRatio: (itemWidth / itemHeight),
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "Nombre:",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 14.0, fontStyle: FontStyle.italic),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "No.Factura:",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 14.0, fontStyle: FontStyle.italic),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Center(
+                      child: Text(
+                        "Zona: ",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 14.0, fontStyle: FontStyle.italic),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Center(
+                      child: Text(
+                        "Vendedor: ",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 14.0, fontStyle: FontStyle.italic),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Center(
+                      child: Text(
+                        "Tipo Compra: ",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 14.0, fontStyle: FontStyle.italic),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Center(
+                      child: Text(
+                        "Fecha de Emision: ",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 14.0, fontStyle: FontStyle.italic),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Center(
+                      child: Text(
+                        "Total Compra: ",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 14.0, fontStyle: FontStyle.italic),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Center(
+                      child: Text(
+                        "Tasa de Cambio: ",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 14.0, fontStyle: FontStyle.italic),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
           Divider(),
           Container(
             child: Center(
